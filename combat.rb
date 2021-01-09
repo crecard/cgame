d6df11d887b9c4a7553aa553b9625daf555eb434
tzwm <wangjunzhe4444@gmail.com>
2021-01-10 01:40:42 +0800

    Update the Player class

--- combat.rb
+++ combat.rb
@@ -2,11 +2,11 @@ ROLE_NUM = 2
 SIDE_NUM = 2
 HAND_CARD_NUM = 4
 
-class Game
+class Combat
   attr_accessor :roles
-  attr_reader :cards_pool, :turn_num, :role_num
+  attr_reader :turn_num, :role_num
 
-  def initialize(role_num = ROLE_NUM, cards_pool)
+  def initialize(role_num = ROLE_NUM)
     @role_num = role_num
     @role_num.times do |i|
       @roles[i % SIDE_NUM][i / SIDE_NUM] = Role.new
@@ -14,12 +14,8 @@ class Game
     @turn_num = 0
   end
 
-  def game_init
+  def combat_init
     deal
-  end
-
-  def fight
-
   end
 
   def who_win?
@@ -37,12 +33,12 @@ class Game
   end
 
   # 发牌
-  def deal
-    @role_num.times do |i|
-      cards = HAND_CARD_NUM.map { cards_pool.sample }
-      role_by_index[i].cards = cards
-    end
-  end
+#  def deal
+    #@role_num.times do |i|
+      #cards = HAND_CARD_NUM.map { cards_pool.sample }
+      #role_by_index[i].cards = cards
+    #end
+  #end
 
   def role_by_index(index)
     @roles[index % SIDE_NUM][i / SIDE_NUM]
--- player.rb
+++ player.rb
@@ -1,15 +1,23 @@
-MAX_HEALTH = 10
-MAX_ENERGY = 10
+INIT_HEALTH = 100
+INIT_ENERGY = 3
+INIT_BLOCK = 0
 
 class Player
   attr_accessor :health, :energy, :block
-  attr_accessor :hand_deck
+  attr_accessor :decks
 
-  def initialize(health = MAX_HEALTH, energy = MAX_ENERGY)
+  def initialize(health = INIT_HEALTH, energy = INIT_ENERGY, block = INIT_BLOCK)
     @health = health
     @energy = energy
-    @block = 0
-    @hand_deck = []
+    @block = block
+
+    @decks = {
+      built: [],
+      unused: [],
+      hand: [],
+      discarded: [],
+      depleted: []
+    }
   end
 
   def is_dead?
@@ -21,17 +29,17 @@ class Player
   end
 
   def has_energy?(fee = 1)
-    @fee >= fee
+    @energy >= fee
   end
 
   def get_damage(damage)
-    if block >= damage
-      block -= damage
+    if @block >= damage
+      @block -= damage
       return
     end
 
-    damage -= block
-    block = 0
-    health -= damage
+    damage -= @block
+    @block = 0
+    @health -= damage
   end
 end
