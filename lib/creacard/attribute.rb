7ab52864a400fdfe8d475416ffbb7f82cbe289c2
wuyong <wuyong.44@bytedance.com>
2021-01-24 18:32:07 +0800

    Add Attribute class

---
+++ lib/creacard/attribute.rb
@@ -0,0 +1,15 @@
+class Creacard::Attribute
+  ACT_TARGET_TYPES = [
+    :enemy_all,
+    :enemy_single,
+    :friend_all,
+    :friend_single,
+    :myself
+  ]
+
+  def need_targets
+  end
+
+  def act
+  end
+end
