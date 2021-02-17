4cac92627a69953ad684509a71e9264874d1135a
tzwm <wangjunzhe4444@gmail.com>
2021-02-17 21:27:18 +0800

    Add the Barricade card, attribute and status

---
+++ lib/creacard/attributes/barricade_attribute.rb
@@ -0,0 +1,12 @@
+class Creacard::BarricadeAttribute < Creacard::Attribute
+  def description
+    'Block no longer expires at the start of your turn.'
+  end
+
+  def act!(owner:, targets: [])
+    case @target_range
+    when :single
+      targets[0].update_status!(Creacard::BarricadeStatus, @value)
+    end
+  end
+end
---
+++ lib/creacard/statuses/barricade_status.rb
@@ -0,0 +1,15 @@
+class Creacard::BarricadeStatus < Creacard::Status
+  def new_turn_act!(owner:)
+    owner.is_block_expire = false
+  end
+
+  class << self
+    def name
+      'Barricade'
+    end
+
+    def description
+      'Block no longer expires at the start of your turn.'
+    end
+  end
+end
